3e0168ad319b5dd83acce5608d25f0a0f0a2d646
ArunDtej <arunch99129@gmail.com>
2024-06-05 14:31:20 +0530

    Stable set up

---
+++ app/src/main/java/com/bruhdev/myapplication/UiManagers/Messenger.java
@@ -0,0 +1,16 @@
+package com.bruhdev.myapplication.UiManagers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bruhdev.myapplication.R;
+
+public class Messenger extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messenger);
+    }
+}
